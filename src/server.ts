a9724d4ca0be331e86dbe0c1c5a9715f78b14cd9
prtpj1 <prtpj1@gmail.com>
2023-10-14 21:55:24 -0300

    :yum: feat: add route get all prompts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import fastifyCors from '@fastify/cors';
 import fastify from 'fastify';
+import { prisma } from './libs/script';
+import { getAllPromptsRoute } from './routes/get-all-prompts';
 
 const app = fastify()
 const port = parseInt(process.env.PORT || '3333')
@@ -7,6 +9,8 @@ const port = parseInt(process.env.PORT || '3333')
 app.register(fastifyCors, {
     origin: '*',
 })
+
+app.register(getAllPromptsRoute)
 
 app.get('/', () => {
     return 'Hello World!'
